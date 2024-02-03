35f48c9dafee32d0fcf20a0dce141b071a05a2ed
tchering <sonam.sherpa.lama@gmail.com>
2024-02-03 20:14:46 +0100

    added another bootstrap

--- View/base.html.php
+++ View/base.html.php
@@ -18,6 +18,13 @@
     <script src="service/script_navbar.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
     <script src="Service/myScript.js" defer></script>
+<!-- for alternative bootstrap -->
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css" rel="stylesheet" />
     <title>Document</title>
 </head>
 
--- View/client/listClient.html.php
+++ View/client/listClient.html.php
@@ -1,3 +1,103 @@
+<!-- <table class="table align-middle mb-0 bg-white">
+    <div class="d-flex justify-content-between align-items-center">
+        <a href="client&action=insert" button type="button" class="btn btn-secondary my-2">CREER</button><i class="fas fa-plus"></i></a>
+        <div class="d-flex justify-content-center align-items-center mx-3">
+            <input id="mot" name="mot" onkeydown="touch(event)" type="search" class="search" placeholder="Recherche">
+            <a href="javascript:search()" class="search-logo text-dark"><i class="fa fa-search fa-lg"></i></a>
+        </div>
+        
+
+    </div>
+    <thead class="bg-light">
+        <tr>
+            <th>Nom</th>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Position</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody id="tbody_client">
+    </tbody>
+</table>
+
+<script>
+    let listClients = <?= json_encode($listClients); ?>;
+    allClients(listClients);
+
+    function allClients(tableName) {
+        let template = listClients.map((client) => {
+            return `
+            <tr>
+      <td>
+        <div class="d-flex align-items-center">
+          <img
+              src="public/upload/${client.photo}"
+              alt=""
+              style="width: 45px; height: 45px"
+              class="rounded-circle"
+              />
+          <div class="ms-3">
+            <p class="fw-bold mb-1">${client.nom} ${client.prenom}</p>
+            <p class="text-muted mb-0">${client.email}</p>
+          </div>
+        </div>
+      </td>
+      <td>
+        <p class="fw-normal mb-1">Software engineer</p>
+        <p class="text-muted mb-0">IT department</p>
+      </td>
+      <td>
+        <span class="badge badge-success rounded-pill d-inline">Active</span>
+      </td>
+      <td>Senior</td>
+      <td>
+      <div class="button d-flex justify-content-between my-3">
+                        <a href="client&action=afficher&id=${client.id}" class="btn btn-sm btn-secondary mx-2" ><i class="fas fa-eye"></i></a>
+                        <a href="client&action=modifier&id=${client.id}" class="btn btn-sm btn-success mx-2"><i class = "fas fa-pen"></i></a>
+                        <a href="javascript:void(0);" class="btn btn-sm btn-danger mx-2" onclick="supprimer(${client.id})"><i class="fas fa-trash"></i></a> 
+                    </div>
+      </td>
+    </tr>
+    <tr>
+     
+            `;
+        }).join(' ');
+
+        document.getElementById('tbody_client').innerHTML = template;
+        let count = listClients.length;
+        document.getElementById('count').innerHTML = `<h3>Total Client : ${count}</h3>`;
+    }
+    const supprimer = (id) => {
+        let confirmation = confirm('Voulez-vous vraiment supprimer ce client?');
+        if (confirmation) {
+            document.location.href = `client&action=supprimer&id=${id}`;
+        }
+    }
+
+    const touch = (event) => {
+        if (event.key == "Enter") {
+            search();
+        }
+    }
+
+
+
+    function search() {
+        let xhr = new XMLHttpRequest();
+        let mot = document.getElementById('mot').value;
+        xhr.open('GET', `client&action=search&mot=${mot}`);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                listClients = JSON.parse(xhr.responseText);
+                allClients(listClients);
+            }
+        }
+        xhr.send();
+    }
+</script> -->
+
+
 <head>
     <link rel="stylesheet" href="public/css/tableclient.css">
 </head>
@@ -90,9 +190,3 @@
         xhr.send();
     }
 </script>
-
-<!-- <div class="button d-flex justify-content-between my-3">
-                        <a href="client&action=afficher&id=${client.id}" class="btn btn-sm btn-secondary mx-2" ><i class="fas fa-eye"></i></a>
-                        <a href="client&action=modifier&id=${client.id}" class="btn btn-sm btn-success mx-2"><i class = "fas fa-pen"></i></a>
-                        <a href="javascript:void(0);" class="btn btn-sm btn-danger mx-2" onclick="supprimer(${client.id})"><i class="fas fa-trash"></i></a> 
-                    </div> -->
